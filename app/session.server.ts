1ce5b60f13cdb840bd82c8c78ee75d25567073c0
Thomas Beirne <thomas.beirne@gmail.com>
2022-01-19 15:50:36 -0800

    Add some more session stuff

--- app/session.server.ts
+++ app/session.server.ts
@@ -1,24 +1,64 @@
 import { createCookieSessionStorage, redirect } from "remix";
+import { db } from "~/db.server";
 
 const sessionSecret = process.env.SESSION_SECRET;
 if (!sessionSecret) throw new Error("SESSION_SECRET must be set");
 
-const storage = createCookieSessionStorage({
-  cookie: {
-    name: "rolldex_session",
-    secure: process.env.NODE_ENV === "production",
-    secrets: [sessionSecret],
-    sameSite: "lax",
-    path: "/",
-    maxAge: 60 * 60 * 24 * 30,
-    httpOnly: true,
-  },
-});
+const { getSession, commitSession, destroySession } =
+  createCookieSessionStorage({
+    cookie: {
+      name: "rolldex_session",
+      secure: process.env.NODE_ENV === "production",
+      secrets: [sessionSecret],
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 30,
+      httpOnly: true,
+    },
+  });
 
 export async function createUserSession(userId: string, redirectTo: string) {
-  const session = await storage.getSession();
+  const session = await getSession();
   session.set("userId", userId);
   return redirect(redirectTo, {
-    headers: { "Set-Cookie": await storage.commitSession(session) },
+    headers: { "Set-Cookie": await commitSession(session) },
+  });
+}
+
+export function getUserSession(request: Request) {
+  return getSession(request.headers.get("Cookie"));
+}
+
+export async function getUserId(request: Request) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+
+  if (!userId || typeof userId !== "string") return null;
+  return userId;
+}
+
+export async function requireUserId(request: Request) {
+  let session = await getUserSession(request);
+  let userId = session.get("userId");
+  if (!userId || typeof userId !== "string") throw redirect("/login");
+  return userId;
+}
+
+export async function getUser(request: Request) {
+  let userId = await getUserId(request);
+  if (typeof userId !== "string") return null;
+
+  try {
+    let user = await db.user.findUnique({ where: { id: userId } });
+    return user;
+  } catch {
+    throw logout(request);
+  }
+}
+
+export async function logout(request: Request) {
+  let session = await getSession(request.headers.get("Cookie"));
+  return redirect("/login", {
+    headers: { "Set-Cookie": await destroySession(session) },
   });
 }
